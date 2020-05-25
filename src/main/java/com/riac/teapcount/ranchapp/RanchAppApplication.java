75cd00b759bad3da1c20029565a37025b6a4f698
zaragozar83 <zaragozar83@gmail.com>
2020-05-25 18:15:26 -0400

    Fist commit start

---
+++ src/main/java/com/riac/teapcount/ranchapp/RanchAppApplication.java
@@ -0,0 +1,13 @@
+package com.riac.teapcount.ranchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riac/teapcount/ranchapp/RanchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riac.teapcount.ranchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
